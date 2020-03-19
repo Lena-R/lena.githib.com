adcf03bf5fd42c1f5d1dcb10b71eb0dd65529358
Lena <lena.boruslav@gmail.com>
2020-03-19 23:08:57 +0200

    moviesapp

---
+++ moviesapp/precache-manifest.107d7e014321af296c66ceaf1a42637d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc48944d6dfdcd9035fd0318a0381d2f",
+    "url": "/lenadgit.github.io/reactwarriors-movieapp/index.html"
+  },
+  {
+    "revision": "f8842e037463bea04415",
+    "url": "/lenadgit.github.io/reactwarriors-movieapp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a18aa23d5b526cc37498",
+    "url": "/lenadgit.github.io/reactwarriors-movieapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f8842e037463bea04415",
+    "url": "/lenadgit.github.io/reactwarriors-movieapp/static/js/2.1d4bbde3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/lenadgit.github.io/reactwarriors-movieapp/static/js/2.1d4bbde3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a18aa23d5b526cc37498",
+    "url": "/lenadgit.github.io/reactwarriors-movieapp/static/js/main.70b16e76.chunk.js"
+  },
+  {
+    "revision": "9c79765f31a4a6a3e2e4",
+    "url": "/lenadgit.github.io/reactwarriors-movieapp/static/js/runtime-main.68f78771.js"
+  }
+]);
