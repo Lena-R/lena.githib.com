28f7db78e70791c66fb3440f73369e45951e04e1
Lena <lena.boruslav@gmail.com>
2020-08-27 01:37:53 +0300

    add recent blocks

---
+++ nutren/js/slider.js
@@ -0,0 +1,23 @@
+let slider = tns({
+    container: ".navSlider",
+    items: 3,
+    controls: true,
+    navPosition: "bottom",
+    responsive: {
+      300: {
+        items: 1,
+      },
+      500: {
+        items: 1,
+      },
+      768: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+      1100: {
+        items: 1,
+      },
+    },
+  });
